54fe6ff7e9c2fbc05d04101d2e90dacd8512051e
kaitokw <102027784+kaitokw@users.noreply.github.com>
2022-03-21 18:44:40 +0700

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn kkuni.wsgi --log-file-
---
+++ kkuni.py
@@ -0,0 +1,99 @@
+from flask import Flask, jsonify, render_template, request
+import json
+import numpy as np
+import pandas as pd
+import geopy.distance as ps
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ImageSendMessage, StickerSendMessage, AudioSendMessage
+)
+from linebot.models.template import *
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+
+app = Flask(__name__)
+
+lineaccesstoken = 'F7p77Oy6+pQ7D3zh+dJv1hHWg2Fh2FmRnrRneZoz6OP1e1PFk/F0Wv0jYOAhx7hpF63nOuFCNnFaqavShreVny/b1g5+CAOksfaNSj6ES4ZTA20cXL/xUlWRDq+Oa2zW40IPhJ+qeEwhpOjBrG74KQdB04t89/1O/w1cDnyilFU='
+line_bot_api = LineBotApi(lineaccesstoken)
+
+location = pd.read_excel('D:\ProIE\KKUni\Location.xlsx')
+
+@app.route('/webhook', methods=['POST'])
+def callback():
+    json_line = request.get_json(force=False,cache=False)
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    no_event = len(decoded['events'])
+    for i in range(no_event):
+        event = decoded['events'][i]
+        event_handle(event)
+    return '',200
+
+
+def event_handle(event):
+    print(event)
+    try:
+        userId = event['source']['userId']
+    except:
+        print('error cannot get userId')
+        return ''
+
+    try:
+        rtoken = event['replyToken']
+    except:
+        print('error cannot get rtoken')
+        return ''
+    try:
+        msgId = event["message"]["id"]
+        msgType = event["message"]["type"]
+    except:
+        print('error cannot get msgID, and msgType')
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+        return ''
+
+    if msgType == "text":
+        msg = str(event["message"]["text"])
+        replyObj = TextSendMessage(text=msg)
+        line_bot_api.reply_message(rtoken, replyObj)
+
+    if msgType == "location":
+        lat = event["message"]["latitude"]
+        lng = event["message"]["longitude"]
+        txtresult = handle_location(lat,lng,location,3)
+        replyObj = TextSendMessage(text=txtresult)
+        line_bot_api.reply_message(rtoken, replyObj)
+    else:
+        sk_id = np.random.randint(1,17)
+        replyObj = StickerSendMessage(package_id=str(1),sticker_id=str(sk_id))
+        line_bot_api.reply_message(rtoken, replyObj)
+    return ''
+
+def handle_location(lat,lng,cdat,topK):
+    result = getdistace(lat, lng,cdat)
+    result = result.sort_values(by='km')
+    result = result.iloc[0:topK]
+    txtResult = ''
+    for i in range(len(result)):
+        kmdistance = '%.1f'%(result.iloc[i]['km'])
+        station = str(result.iloc[i]['Station'])
+        txtResult = txtResult + 'ห่าง %s กิโลเมตร\n%s\n\n'%(kmdistance,station)
+    return txtResult[0:-2]
+
+
+def getdistace(latitude, longitude,cdat):
+  coords_1 = (float(latitude), float(longitude))
+  ## create list of all reference locations from a pandas DataFrame
+  latlngList = cdat[['Latitude','Longitude']].values
+  ## loop and calculate distance in KM using geopy.distance library and append to distance list
+  kmsumList = []
+  for latlng in latlngList:
+    coords_2 = (float(latlng[0]),float(latlng[1]))
+    kmsumList.append(ps.vincenty(coords_1, coords_2).km)
+  cdat['km'] = kmsumList
+  return cdat
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
